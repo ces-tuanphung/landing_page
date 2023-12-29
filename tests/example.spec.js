684fa1aa99f023b08399e8ae2dfdfefa750e96a4
ces-tuanphung <tuan.phung@codeenginestudio.com>
2023-12-29 13:23:28 +0700

    next2

--- tests/example.spec.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-const { test, expect } = require("@playwright/test");
-
-test("home page", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-  expect(await page.screenshot({ fullPage: true })).toMatchSnapshot("home.png");
-});
