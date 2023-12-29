0f0e45b17c430f00e179f2651e8df09979f40bbf
ces-tuanphung <tuan.phung@codeenginestudio.com>
2023-12-29 13:41:39 +0700

    next3

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -27,21 +27,21 @@ export const Navigation = (props) => {
           id="bs-example-navbar-collapse-1"
         >
           <ul className="nav navbar-nav navbar-right">
-            {/* <li>
+            <li>
               <a href="#features" className="page-scroll">
                 Features
               </a>
-            </li> */}
+            </li>
             <li>
               <a href="#about" className="page-scroll">
                 About
               </a>
             </li>
-            {/* <li>
+            <li>
               <a href="#services" className="page-scroll">
                 Services
               </a>
-            </li> */}
+            </li>
             <li>
               <a href="#portfolio" className="page-scroll">
                 Gallery 
